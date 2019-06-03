2c78a2cbfeccf3309a8429a7b0c35f482663c734
victor <vili@nvidia.com>
2019-06-03 23:21:45 +0800

    add cl prefix

--- citools/run_sample.sh
+++ citools/run_sample.sh
@@ -1,6 +1,8 @@
 TOP=`pwd`/..
 cd $TOP
 timestamp=`date +%Y%m%d%H%M%S`
+cl=`git rev-parse HEAD|cut -c1-7`
+timestamp="$cl-$timestamp"
 docker run  --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $TOP/results/:/Neuralpersistence/results/ neuralpersistence python3 -u run_experiments.py 2>&1 | tee /Neuralpersistence/results/run_experiments_$timestamp.log
 
 docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $TOP/results/:/Neuralpersistence/results/ neuralpersistence python3 combine_runs.py results/runs/* --output results/combined_runs.csv 2>&1 |tee /Neuralpersistence/results/combine_runs_$timestamp.log
